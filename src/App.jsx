2872fb2bcb09f4e01eaa786efa750f823b7bbdd5
danishinnovacion <danish@techinnovacion.com>
2026-04-08 16:43:08 +0530

    chore: enable GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   return (
     <div className="app">
       <div className="todo-container">
-        <h1>React Todo App</h1>
+        <h1>React Todo App for CICD with using Githube Action</h1>
 
         <div className="input-group">
           <input
